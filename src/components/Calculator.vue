17cc0b1cb34456b717be6a24df4ce2c65931b83f
lilianzheng <lilianzheng@yy.com>
2020-07-01 18:08:35 +0800

    feat: 新增Calculator 组件，实现一个简单的计算器，输入两个数字，触发求和按钮后展示相加后的值。同时这个组件接受一个props: salt，会影响求和计算 这里用到了我们文档里提到的几点 1.  setup()是composition api的入口，所有的composition api都在这里写 2.  setup(props)里的props就是组件的props 3.  通过ref定义了3个响应式的数据， num1,num2,result

---
+++ src/components/Calculator.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <div class="content">
+      <input type="number" v-model="num1" />
+      <span>+</span>
+      <input type="number" v-model="num2" />
+      <span>=</span>
+      <input type="number" v-model="result" />
+    </div>
+    <button class="btn" @click="add2">求和</button>
+  </div>
+</template>
+
+<script lang="ts">
+// export declare function ref<T extends object>(value: T): T extends Ref ? T : Ref<UnwrapRef<T>>;
+import { ref } from "vue"
+
+
+export default {
+  name: "Calculator",
+  props: {
+    salt: {type: Number, default: 100}
+  },
+  setup(props) {
+    const num1 = ref(0)
+    const num2 = ref(1)
+    const result = ref(1)
+
+    const add2 = ()=>{result.value = num1.value + num2.value + props.salt}
+
+    return {
+      num1,
+      num2,
+      result,
+      add2
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.content {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  display: inline-block;
+  width: 100px;
+  height: 40px;
+  text-align: center;
+  margin: 10px;
+}
+.btn {
+  width: 100px;
+  height: 30px;
+  margin: 0 auto;
+  // border: 1px solid paleturquoise;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+</style>
